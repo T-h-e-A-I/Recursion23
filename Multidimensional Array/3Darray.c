dc7ce149fe83d839acf7f43c34791726c2787694
Awesh Islam <aweshrumc@gmail.com>
2024-05-25 00:52:23 +0600

    finished multidimensional array

---
+++ Multidimensional Array/3Darray.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main(){
+    int hotel[3][4][7] = {
+        {{1, 0, 1, 1, 0, 1, 1},
+         {0, 1, 0, 0, 1, 1, 0},
+         {1, 1, 0, 0, 0, 1, 0},
+         {1, 0, 1, 0, 0, 1, 1}},
+        {{1, 1, 1, 1, 1, 1, 1},
+         {1, 1, 1, 1, 1, 1, 1},
+         {0, 0, 0, 0, 0, 0, 0},
+         {1, 0, 0, 0, 0, 0, 0}},
+        {{1, 1, 1, 1, 1, 0, 0},
+         {1, 0, 1, 0, 1, 0, 1},
+         {1, 0, 1, 0, 0, 1, 1},
+         {1, 0, 1, 0, 0, 1, 1}}
+    };
+    int floor,room,day;
+    printf("Which floor do you want stay in: ");
+    scanf("%d",&floor);
+    printf("Which room do you want to stay in: ");
+    scanf("%d",&room);
+    printf("Which day you want to stay at: ");
+    scanf("%d",&day);
+    if(hotel[floor][room][day] == 1) printf("Room Available.");
+    else printf("Room not available.");
+}
---
+++ Multidimensional Array/salesProblem.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main(){
+    int prodAjan = 100, prodAfeb = 150, prodAmar = 130, prodAapr = 120;
+    int prodBjan = 210, prodBfeb = 220, prodBmar = 230, prodBapr = 240;
+    int prodCjan = 300, prodCfeb = 310, prodCmar = 320, prodCapr = 330;
+}
