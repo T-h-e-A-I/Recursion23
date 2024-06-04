b451543f89573c6985c4b4fc740d1655c5c3f10b
Awesh Islam <aweshrumc@gmail.com>
2024-06-05 00:02:12 +0600

    added lecture slides and practice problems of classic problems class

--- Classic Problems/Fibonacci.c
+++ Classic Problems/Fibonacci.c
@@ -6,10 +6,10 @@ long long fib(long long n){
     else return arr[n] = (fib(n-1)+fib(n-2));
 }
 int main(){
-    long long n;
     for(int i = 0;i < 1000;i++) arr[i] = -1;
     arr[0] = 0;
     arr[1] = 1;
+    long long n;
     printf("Please enter a number: ");
     scanf("%lld",&n);
     printf("The %lldth fibonacci number is: %lld\n",n,fib(n));
